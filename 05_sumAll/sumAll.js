68f4a3e7577a2f1bcb1f9a84f169ff4f2bef57a9
Ran <96964503+rndall@users.noreply.github.com>
2024-12-25 16:45:10 +0800

    05_sumAll: Write sumAll() function and test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if ((!Number.isInteger(num1) || !Number.isInteger(num2)) || (num1 < 0 || num2 < 0)) {
+    return 'ERROR'
+  }
 
+  let start;
+  let end;
+  if (num1 < num2) {
+    start = num1
+    end = num2
+  } else {
+    start = num2
+    end = num1
+  }
+
+  let sum = 0
+  for (start; start <= end; start++) {
+    sum += start
+  }
+  return sum
 };
 
 // Do not edit below this line
