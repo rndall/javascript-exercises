b7302f64f4e5058877d7e2b8969f5a98fdb0d739
Ran <96964503+rndall@users.noreply.github.com>
2024-12-28 23:26:45 +0800

    08_calculator: Write and test factorial()

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,7 +18,15 @@ const power = function (num, power) {
 	return num ** power;
 };
 
-const factorial = function () {};
+const factorial = function (num) {
+	if (num === 0) return 1;
+
+	let product = 1;
+	for (let i = 1; i <= num; i++) {
+		product *= i;
+	}
+	return product;
+};
 
 // Do not edit below this line
 module.exports = {
