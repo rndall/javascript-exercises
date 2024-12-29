dcc4df14f5aec725662dcabd361d8c4b35880d20
Ran <96964503+rndall@users.noreply.github.com>
2024-12-29 17:35:34 +0800

    10_fibonacci: Write fibonacci() and pass first 6 tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+	let start = 0;
+	let lastNum = 1;
+	for (let i = 0; i < index; i++) {
+		const temp = lastNum;
+		lastNum += start;
+		start = temp;
+	}
+	return start;
 };
 
 // Do not edit below this line
