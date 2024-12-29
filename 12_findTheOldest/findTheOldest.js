3679a6bcfbca75e813da8cf98c73429f1d6cf962
Ran <96964503+rndall@users.noreply.github.com>
2024-12-29 18:05:39 +0800

    12_findTheOldest: Write findTheOldest() and pass first test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+	arr.sort((a, b) => {
+		firstAge = a.yearOfDeath - a.yearOfBirth;
+		secondAge = b.yearOfDeath - b.yearOfBirth;
+		return secondAge - firstAge;
+	});
 
+	return arr[0];
 };
 
 // Do not edit below this line
